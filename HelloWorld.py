1539d377e6a83e17f8ad4263a3f0a6fb195d7493
MRodriguez-M <melissa.rodriguez.murguia@gmail.com>
2019-09-17 18:41:12 -0500

    updated prompt

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,16 @@
 """
 (1) Write a program that outputs the following.
+
+Hello world!
+
+(2) Update to output the following.
+
+Hello world!
+How are you?
+
+(3) Finally, update to output the following.
+
+Hello world!
+How are you?
+   (I'm fine).
 """
