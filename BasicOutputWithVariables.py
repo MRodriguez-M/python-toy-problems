5622f465bd078925e676707a11e4f2af949d2496
MRodriguez-M <melissa.rodriguez.murguia@gmail.com>
2019-09-17 19:05:14 -0500

    completed step 1 of BasicOutputWithVariables

---
+++ BasicOutputWithVariables.py
@@ -0,0 +1,27 @@
+"""
+(1) Output the user's input.
+
+Enter integer: 4
+You entered: 4
+
+(2) Extend to output the input squared and cubed.
+
+Enter integer: 4
+You entered: 4
+4 squared is 16 
+And 4 cubed is 64 !!
+
+(3) Extend to get a second user input into userNum2. Output sum and product.
+
+Enter integer: 4
+You entered: 4
+4 squared is 16 
+And 4 cubed is 64 !!
+Enter another integer: 5
+4 + 5 is 9
+4 * 5 is 20
+"""
+
+userNum = int(input("Enter integer: "))
+
+print("You entered:", userNum)
