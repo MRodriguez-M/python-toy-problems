e173e8cde380e93e6f816b1497c1e53d443ef6c9
MRodriguez-M <melissa.rodriguez.murguia@gmail.com>
2019-09-26 00:35:34 -0500

    completed step 1 of AsciiArt

---
+++ AsciiArt.py
@@ -0,0 +1,24 @@
+"""
+(1) Output this tree.
+
+   *
+  ***
+ *****
+*******
+  ***
+
+(2) Below the tree (with two blank lines), output this cat.
+
+/\   /\
+  o o
+ =   =
+  ---
+"""
+
+print("   *")
+print("  ***")
+print(" *****")
+print("*******")
+print("  ***")
+
+print("/\\")
