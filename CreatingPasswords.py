40439b5eb8a0448ee533851f4c60fcdbdc765e6d
MRodriguez-M <melissa.rodriguez.murguia@gmail.com>
2019-10-17 19:36:26 -0500

    completed step 1 of CreatingPasswords

---
+++ CreatingPasswords.py
@@ -0,0 +1,51 @@
+"""
+(1) Prompt the user to enter two words and a number, storing each into separate variables.
+Then, output those three values on a single line separated by a space.
+
+Enter favorite color:
+yellow                                                                                                                                                         
+Enter pet's name:
+Daisy                                                                                                                                                
+Enter a number:
+6
+
+yellow Daisy 6
+
+(2) Output two passwords using a combination of the user input.
+Format the passwords as shown below.
+
+Enter favorite color:
+yellow                                                                                                                                                         
+Enter pet's name:
+Daisy                                                                                                                                                
+Enter a number:
+6
+
+yellow Daisy 6
+
+First password: yellow_Daisy
+Second password: 6yellow6
+
+(3) Output the length of each password (the number of characters in the strings).
+
+Enter favorite color:
+yellow                                                                                                                                                         
+Enter pet's name:
+Daisy                                                                                                                                                
+Enter a number:
+6
+
+yellow Daisy 6
+
+First password: yellow_Daisy
+Second password: 6yellow6
+
+Number of characters in yellow_Daisy: 12
+Number of characters in 6yellow6: 8
+"""
+
+favoriteColor = input("Enter favorite color:\n")
+petName = input("Enter pet's name:\n")
+number = input("Enter a number:\n")
+
+print("\n" + favoriteColor, petName, number)
