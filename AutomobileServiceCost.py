c8216a6cce4b80f70aac20f69e8836cb4f9cc084
MRodriguez-M <melissa.rodriguez.murguia@gmail.com>
2019-10-30 15:48:45 -0600

    successfully output price of requested service in AutomobileServiceCost

--- AutomobileServiceCost.py
+++ AutomobileServiceCost.py
@@ -17,7 +17,16 @@ Car wash -- $7
 If the user enters a service that is not listed above, then output the following error message:
 Error: Requested service is not recognized.
 """
+# Dictionary for available services
+services = {
+    "Oil change": 35,
+    "Tire rotation": 19,
+    "Car wash": 7
+}
 
 # Prompt user for service desired and output user input
 autoService = input("Enter desired service: ")
 print("You entered:", autoService)
+
+if autoService in services:
+    print("\nCost of", autoService.lower() + ": $%s" % services[autoService])
