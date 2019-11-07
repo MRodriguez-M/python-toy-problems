f8a8aebf4888ec5ca236ccbf579a8fe78db9cdc7
MRodriguez-M <melissa.rodriguez.murguia@gmail.com>
2019-11-07 15:12:02 -0600

    added comments to FoodReceipt

--- FoodReceipt.py
+++ FoodReceipt.py
@@ -56,28 +56,38 @@ Total cost: $ 20.0
 Total with tip: $ 23.0
 """
 
+# Prompt user for information about food item 1
 itemName = input("Enter food item name: ")
 itemPrice = float(input("Enter item price: "))
 itemQuantity = int(input("Enter item quantity: "))
+
+# Calculate cost for food item 1 based on user input
 itemTotal = itemQuantity * itemPrice
 
+# Output receipt for food item 1
 print("\nRECEIPT")
 print(itemQuantity, itemName, "@ $", itemPrice, "= $", itemTotal)
 print("Total cost: $", itemTotal)
 
+# Prompt user for information about food item 2
 itemName2 = input("\n\nEnter second food item name: ")
 itemPrice2 = float(input("Enter item price: "))
 itemQuantity2 = int(input("Enter item quantity: "))
+
+# Calculate cost for food item 2 based on user input and total cost of both items
 itemTotal2 = itemQuantity2 * itemPrice2
 totalCost = itemTotal + itemTotal2
 
+# Output receipt for food item 2
 print("\nRECEIPT")
 print(itemQuantity, itemName, "@ $", itemPrice, "= $", itemTotal)
 print(itemQuantity2, itemName2, "@ $", itemPrice2, "= $", itemTotal2)
 print("Total cost: $", totalCost)
 
+# Calculate gratuity of total cost of both items and find grand total
 gratuity =  totalCost * .15
 grandTotal = totalCost + gratuity
 
+# Output 15% gratuity fee and grand total consisting of total cost with gratuity added on
 print("\n15% gratuity: $", gratuity)
 print("Total with tip: $", grandTotal)
