6bebe313e4d365095a87cc07e862335426c04c2f
MRodriguez-M <melissa.rodriguez.murguia@gmail.com>
2019-12-19 00:58:07 -0600

    completed step 1 of TextAnalyzerAndModifier

---
+++ TextAnalyzerAndModifier.py
@@ -0,0 +1,28 @@
+"""
+(1) Prompt the user to enter a string of their choosing.
+Output the string.
+
+Enter a sentence or phrase: The only thing we have to fear is fear itself.
+You entered: The only thing we have to fear is fear itself.
+
+(2) Complete a function which returns the number of characters in the user's string.
+
+(3) Calling the function and output the returned result.
+
+Enter a sentence or phrase: The only thing we have to fear is fear itself.
+You entered: The only thing we have to fear is fear itself.
+
+Number of characters: 46
+
+(4) Extend the program further by implementing a function that outputs the string's characters except for whitespace.
+
+Enter a sentence or phrase: The only thing we have to fear is fear itself.
+You entered: The only thing we have to fear is fear itself.
+
+Number of characters: 46
+String with no whitespace: Theonlythingwehavetofearisfearitself.
+"""
+
+# Prompting user for string and printing user input
+userString = input("Enter a sentence or phrase: ")
+print("You entered:", userString)
