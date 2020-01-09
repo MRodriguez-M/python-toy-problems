4aaed23a17889b009700cc1b92d0415744590b99
unknown <melissa.rodriguez.murguia@gmail.com>
2020-01-09 11:28:17 -0600

    added steps to expand functionality of OnlineShoppingCart

--- OnlineShoppingCart.py
+++ OnlineShoppingCart.py
@@ -33,6 +33,140 @@ Chocolate Chips 1 @ $3 = $3
 Bottled Water 10 @ $1 = $10
 
 Total: $13
+
+(4) Extend the ItemToPurchase class to contain a new attribute.
+item_description (string) - Set to "none" in default constructor 
+Implement the following method for the ItemToPurchase class.
+print_item_description() - Has an ItemToPurchase parameter
+
+Ex. of print_item_description() output:
+
+Bottled Water: Deer Park, 12 oz.
+
+(5) Build the ShoppingCart class with the following data attributes and related methods.
+Parameterized constructor which takes the customer name and date as parameters
+
+Attributes
+customer_name (string) - Initialized in default constructor to "none"
+current_date (string) - Initialized in default constructor to "January 1, 2016"
+cart_items (list)
+
+Methods
+add_item()
+Adds an item to cart_items list.
+Has parameter ItemToPurchase.
+Does not return anything.
+remove_item()
+Removes item from cart_items list.
+Has a string (an item's name) parameter.
+Does not return anything.
+If item name cannot be found, output this message: Item not found in cart. Nothing removed.
+modify_item()
+Modifies an item's description, price, and/or quantity.
+Has parameter ItemToPurchase.
+Does not return anything.
+If item can be found (by name) in cart, check if parameter has default values for description, price, and quantity.
+If not, modify item in cart.
+If item cannot be found (by name) in cart, output this message: Item not found in cart. Nothing modified.
+get_num_items_in_cart()
+Returns quantity of all items in cart.
+Has no parameters.
+get_cost_of_cart()
+Determines and returns the total cost of items in cart.
+Has no parameters. 
+print_total()
+Outputs total of objects in cart.
+If cart is empty, output this message: SHOPPING CART IS EMPTY
+print_descriptions()
+Outputs each item's description. 
+
+Ex. of print_total() output:
+
+John Doe's Shopping Cart - February 1, 2016
+Number of Items: 8
+
+Nike Romaleos 2 @ $189 = $378
+Chocolate Chips 5 @ $3 = $15
+Powerbeats 2 Headphones 1 @ $128 = $128
+
+Total: $521
+
+Ex. of print_descriptions() output:
+
+John Doe's Shopping Cart - February 1, 2016
+
+Item Descriptions
+Nike Romaleos: Volt color, Weightlifting shoes
+Chocolate Chips: Semi-sweet
+Powerbeats 2 Headphones: Bluetooth headphones
+
+
+(6) In main section of your code, prompt the user for a customer's name and today's date.
+Output the name and date.
+Create an object of type ShoppingCart.
+
+Enter customer's name: John Doe
+Enter today's date: February 1, 2016
+Customer name: John Doe
+Today's date: February 1, 2016
+
+(7) Implement the print_menu() function. print_menu() has a ShoppingCart parameter, and outputs a menu of options to manipulate the shopping cart.
+Each option is represented by a single character.
+Build and output the menu within the function.
+If the an invalid character is entered, continue to prompt for a valid choice.
+Call print_menu() in the main() function.
+Continue to execute the menu until the user enters q to Quit.
+
+MENU
+a - Add item to cart
+r - Remove item from cart
+c - Change item quantity
+i - Output items' descriptions
+o - Output shopping cart
+q - Quit
+
+Choose an option: 
+
+(8) Implement Output shopping cart menu option.
+
+OUTPUT SHOPPING CART
+John Doe's Shopping Cart - February 1, 2016
+Number of Items: 8
+
+Nike Romaleos 2 @ $189 = $378
+Chocolate Chips 5 @ $3 = $15
+Powerbeats 2 Headphones 1 @ $128 = $128
+
+Total: $521
+
+(9) Implement Output item's description menu option.
+
+OUTPUT ITEMS' DESCRIPTIONS
+John Doe's Shopping Cart - February 1, 2016
+
+Item Descriptions
+Nike Romaleos: Volt color, Weightlifting shoes
+Chocolate Chips: Semi-sweet
+Powerbeats 2 Headphones: Bluetooth headphones
+
+(10) Implement Add item to cart menu option.
+
+ADD ITEM TO CART
+Enter the item name: Nike Romaleos
+Enter the item description: Volt color, Weightlifting shoes
+Enter the item price: 189
+Enter the item quantity: 2
+
+(11) Implement remove item menu option.
+
+REMOVE ITEM FROM CART
+Enter name of item to remove: Chocolate Chips
+
+(12) Implement Change item quantity menu option.
+
+CHANGE ITEM QUANTITY
+Enter the item name: Nike Romaleos
+Enter the new quantity: 3
 """
 
 # Class to manage item name, price, and quantity
