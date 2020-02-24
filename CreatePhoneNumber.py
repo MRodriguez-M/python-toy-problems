47b79e90c6729f7f6c56fca37d98cfedc4ce2d0a
unknown <melissa.rodriguez.murguia@gmail.com>
2020-02-24 17:36:09 -0600

    changed way the list is created in CreatePhoneNumber

--- CreatePhoneNumber.py
+++ CreatePhoneNumber.py
@@ -6,10 +6,10 @@ Write a function that accepts an array of 10 integers (between 0 and 9), that re
 
 # Function to create phone number in proper format
 def createPhoneNumber():
-    numberList = []
-    # For loop to add user input into list
-    for i in userDigits:
-        numberList.append(i)
+    phoneNumber = "("
+    for digit in numberList:
+        phoneNumber = phoneNumber + digit
+    return phoneNumber
 
 # Prompt user for 10 digits that will make up phone number
 userDigits = input("Enter 10-digit phone number (digits only): ")
@@ -18,7 +18,9 @@ userDigits = input("Enter 10-digit phone number (digits only): ")
 if userDigits.isdigit():
     # If statment to check that user input is only 10 digits
     if len(userDigits) == 10:
-        pass
+        # Change user input into list and call function
+        numberList = list(userDigits)
+        print(createPhoneNumber())
     else:
         # Print error message if user input is not exactly 10 digits
         print("Error: Number needs to be 10 digits.")
