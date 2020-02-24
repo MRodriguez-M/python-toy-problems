1cf1428004a4165be13736936e098f2634c563e3
unknown <melissa.rodriguez.murguia@gmail.com>
2020-02-24 17:04:31 -0600

    wrote if statements to check that user input is valid in CreatePhoneNumber

--- CreatePhoneNumber.py
+++ CreatePhoneNumber.py
@@ -13,3 +13,15 @@ def createPhoneNumber():
 
 # Prompt user for 10 digits that will make up phone number
 userDigits = input("Enter 10-digit phone number (digits only): ")
+
+# If statement to check that user input is only digits
+if userDigits.isdigit():
+    # If statment to check that user input is only 10 digits
+    if len(userDigits) == 10:
+        pass
+    else:
+        # Print error message if user input is not exactly 10 digits
+        print("Error: Number needs to be 10 digits.")
+else:
+    # Print error message if user input has characters other than digits
+    print("Error: Number must be digits only.")
